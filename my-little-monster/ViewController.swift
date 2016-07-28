08fc1b10295bdaf986394861b6f78acf79377e5d
FrenchTastic <romainpardon@MacBook-Pro-de-Romain.local>
2016-07-28 19:46:02 +0200

    Added animation
    Add the animation that make the monster move

--- my-little-monster/ViewController.swift
+++ my-little-monster/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monsterImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var imgArray = [UIImage]()
+        for x in 1...4 {
+            let img = UIImage(named: "idle\(x)")
+            imgArray.append(img!)
+        }
+        monsterImg.animationImages = imgArray
+        monsterImg.animationDuration = 0.8
+        monsterImg.animationRepeatCount = 0
+        monsterImg.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
